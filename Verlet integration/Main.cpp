437f2d7c1c5e7a47edfcdf920d12360c766ee3e5
CoolKid1243 <161092074+CoolKid1243@users.noreply.github.com>
2024-06-30 21:54:03 -0400

    Patch-5

--- Verlet integration/Main.cpp
+++ Verlet integration/Main.cpp
@@ -1,8 +1,6 @@
 #include "Application.h"
 #include <memory>
 
-// From dev to dev, log how many days have been waisted on this system here: 5
-
 int main() {
     std::unique_ptr<App> Application = std::make_unique<App>();
     Application->run();
